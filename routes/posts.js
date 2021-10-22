cb39944b3ab35563053ffa98be38bcfdbb227364
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-21 21:11:27 -0700

    added delete a post route

--- routes/posts.js
+++ routes/posts.js
@@ -10,9 +10,9 @@ router.post ('/', async (req, res)=> {
         } catch(err){
             res.status(500).json(err)
         }
-})
+});
 
-//update a post
+//update a postc
 router.put ('/:id', async (req, res) => {
     try {
     const post = await Post.findById(req.params.id);
@@ -28,6 +28,20 @@ router.put ('/:id', async (req, res) => {
 })
 
 //delete a post
+router.delete ('/:id', async (req, res) => {
+    try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+        await post.deleteOne();
+        res.status(200).json('Post has been deleted.')
+    } else {
+        res.status(403).json('You can only delete your posts.')
+        }   
+    } catch(err){
+        res.status(500).json(err);
+    }
+})
+
 //like a post
 //get a post
 //get timeline posts
