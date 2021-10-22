ca0258b2aff7f0968647948af9a3e8d2ebd79724
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-21 21:03:19 -0700

    Add create post route

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Post = require('../models/Post')
 
 //create a post
-router.post('/', async (req, res)=> {
+router.post ('/', async (req, res)=> {
     const newPost = new Post(req.body)
         try {
             const savedPost = await newPost.save();
@@ -11,5 +11,25 @@ router.post('/', async (req, res)=> {
             res.status(500).json(err)
         }
 })
+
+//update a post
+router.put ('/:id', async (req, res) => {
+    try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+        await post.updateOne({$set: req.body})
+        res.status(200).json('Post has been updated.')
+    } else {
+        res.status(403).json('You can only update your posts.')
+        }   
+    } catch(err){
+        res.status(500).json(err);
+    }
+})
+
+//delete a post
+//like a post
+//get a post
+//get timeline posts
 
 module.exports = router;
