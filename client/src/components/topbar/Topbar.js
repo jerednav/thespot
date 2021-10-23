048446af4bbaf66f17ce589236f050b7733972f8
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-23 01:09:13 -0700

    Create topbar components

---
+++ client/src/components/topbar/Topbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./topbar.css"
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo"> The Spot</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                   
+
+                </div>
+            </div>
+            <div className="topbarRight"></div>
+        </div>
+    )
+}
