2c3eab10c14e5b9e6a855664ee0d5a799599402c
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-24 20:22:05 -0700

    Updated feed component

--- client/src/components/share/Share.js
+++ client/src/components/share/Share.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './share.css'
-import { PermMedia } from '@material-ui/icons'
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons'
 
 export default function share() {
     return (
@@ -12,11 +12,27 @@ export default function share() {
                 </div>
                 <hr className='shareHr' />
                 <div className="shareBottom">
+                    <div className="shareOptions">
                     <div className="shareOption">
                         <PermMedia className='shareIcon' />
                         <span className='shareOptionText'>Photo or Video</span>
                     </div>
+                    <div className="shareOption">
+                        <Label className='shareIcon' />
+                        <span className='shareOptionText'>Tag</span>
+                    </div>
+                    <div className="shareOption">
+                        <Room className='shareIcon' />
+                        <span className='shareOptionText'>Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotions className='shareIcon' />
+                        <span className='shareOptionText'>Feelings</span>
+                    </div>
+                    <button className="shareButton">Share</button>
+                    </div>
                 </div>
+               
             </div>
         </div>
             )
