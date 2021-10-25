954c90dffb72d51e77bd8787dd3f4426d9d7c3ef
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-24 21:17:17 -0700

    Created post/Right side bar components

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './post.css'
+import { MoreVert } from '@material-ui/icons'
+
+export default function Post() {
+    return (
+        <div className='post'>
+             <div className="postWrapper">
+                 <div className="postTop">
+                     <div className="postTopLeft">
+                         <img className='postProfileImg' src="/images/ace.jpeg" alt="" />
+                         <span className="postUsername">Ace Navarro</span>
+                         <span className="postDate">5 mins ago</span>
+                     </div>
+                     <div className="postTopRight">
+                         <MoreVert />
+                     </div>
+                 </div>
+                 <div className="postCenter">
+                     <span className="postText">Hey its my first post!</span>
+                     <img className='postImg' src="images/ace.jpeg" alt="" />
+                 </div>
+                 <div className="postBottom">
+                     <div className="postBottomLeft">
+                         <img src="images/like.png" alt="" />
+                         <img src="images/heart.png" alt="" />
+                         <span className="postLikeCounter">32 people liked it!</span>
+                     </div>
+                     <div className="postBottomRight">
+                         <span className="postCommentText">9 comments</span>
+                     </div>
+                 </div>
+             </div>
+            
+        </div>
+    )
+}
--- client/src/components/rightbar/Rightbar.js
+++ client/src/components/rightbar/Rightbar.js
@@ -4,7 +4,39 @@ import './rightbar.css'
 export default function Rightbar() {
     return (
         <div className='rightbar'>
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className='birthdayImg' src="/images/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Ace</b> has a birthday today.
+                    </span>
+                </div>
+                <img className='rightbarAd' src="/images/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className='rightbarProfileImg' src="/images/ace.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Ace</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className='rightbarProfileImg' src="/images/ace.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Ace</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className='rightbarProfileImg' src="/images/ace.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Ace</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
