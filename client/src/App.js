19c0bf928a6884ab81456fb410646e00a6fdf774
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-26 00:04:24 -0700

    Completed UI with dynamic data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import Login from './components/login/Login';
-import Home from './pages/home/Home'
 import Profile from './pages/profile/Profile';
 
 function App() {
-    return <Login />
+    return <Profile />
 }
 
 export default App;
